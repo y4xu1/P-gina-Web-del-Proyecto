0f3245465d6c89d2c19bd9ca80f1fad40c5d4643
Laura23-g <79876914+Laura23-g@users.noreply.github.com>
2022-02-03 16:48:44 -0500

    PDF

---
+++ HTML/PDFcontratos.php
@@ -0,0 +1,28 @@
+<?php
+
+    include("conexion.php");
+
+?>
+<!DOCTYPE html>
+<html lang="es">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+    </head>
+    <body>
+        <h2>N° <?php echo $configuracion['idcontrato'];?> </h2>
+        <h2>Contrato Individual <?php echo $configuracion['tipoContrato'];?> </h2>
+            
+    <div>
+        <table class='cualquiercosaxdjajjjjjjjjjjajjajajajajajajjajajajajajjaaja'>
+            <tr>
+                
+    </div> 
+
+
+            
+        </table>
+    </body>
+</html>
