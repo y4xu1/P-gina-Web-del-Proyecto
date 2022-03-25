91f7f5e929f4c2043260a32f1aa4acfe186a77c3
Laura23-g <79876914+Laura23-g@users.noreply.github.com>
2022-03-25 14:31:49 -0500

    Formulario dotación

--- HTML/php/formularioDotacion.php
+++ HTML/php/formularioDotacion.php
@@ -1,36 +1,42 @@
-<!-- Vinculacion con la paginas dotacion e induccion -->
 <?php
 
     include("conexion.php");
 
     #Identificar cada variable de vinculacion con la pagina y la base de datos.
     #Insertar a la base de datos los datos ingresados en cada campo del formulario.
+    
+    if(isset($_POST['sendDota'])) {
+        insertar($conn);
+    } 
 
-    function insertar ($conn){
-        $Nombres=$_POST["Nombres"];
-        $docA=$_POST["docAspirante"];
-        $cargo=$_POST["Cargo"];
-        $proceso=$_POST["Proceso"];
-        $fechaE=$_POST["fechaEntrega"];
-        $casco=$_POST["Casco"];
-        $overol=$_POST["Overol"];
-        $botasMaterial["botasMaterial"];
-        $botasCaucho=$_POST["botasCaucho"];
-        $guantesCarnaza=$_POST["guantesCarnaza"];
-        $guantesVaqueta=$_POST["guantesVaqueta"];
-        $guantesNitrilo=$_POST["guantesNitrilo"];
-        $protAuditivo=$_POST["protAuditivo"];
-        $protAuditivoCopa=$_POST["protAuditivoCopa"];
-        $Tapabocas=$_POST["Tapabocas"];
-        $Gafas=$_POST["Gafas"];
-        $Barbuquejo=$_POST["Barbuquejo"];
-        $docRecHum=$_POST["docRecHum"];
-        $firmaResponsable=$_POST["firmaResponsable"];
-        $firmaTrabajador=$_POST["firmaTrabajador"];
-        $fechaFormulario=$_POST["fechaFormulario"];
+    function insertar ($conn) {
+
+        $nombres = $_POST ["nombres"];
+        $docAspirante = $_POST ["docAspirante"];
+        $docRecHum = $_POST["docRecHum"];
+        $cargo = $_POST ["cargo"];
+        $proceso = $_POST ["proceso"];
+        $fehaEntrega = $_POST ["fechaEntrega"];
+        $casco = $_POST ["casco"];
+        $overol = $_POST ["overol"];
+        $botasMaterial = $_POST ["botasMaterial"];
+        $botasCaucho = $_POST ["botasCaucho"];
+        $guantesCarnaza = $_POST ["guantesCarnaza"];
+        $guantesCaucho = $_POST ["guantesCaucho"];
+        $guantesVaqueta = $_POST ["guantesVaqueta"];
+        $guantesNitrilo = $_POST ["guantesNitrilo"];
+        $protAuditivo = $_POST ["protAuditivo"];
+        $protAuditivoCopa = $_POST ["protAuditivoCopa"];
+        $tapabocas = $_POST ["tapabocas"];
+        $gafas = $_POST ["gafas"];
+        $barbuquejo = $_POST ["barbuquejo"];
+        $firmaResponsable = $_POST ["firmaResponsable"];
+        $firmaTrabajador = $_POST ["firmaTrabajador"];
+        $fechaFormulario = $_POST ["fechaFormulario"];
 
 
-        $query_subirDatos= "INSERT INTO formulario_dotacion (nombresCompletos,docAspirante,) VALUES ('$Nombres','$docA','$cargo','$proceso','$fechaE','$casco','$overol','$botasMaterial','$botasCaucho','$guantesCarnaza','')";
+        $query_subirDatos= "INSERT INTO formulario_dotacion (nombresCompletos,docAspirante,docRecHum,cargo,pct,fechaEntrega,casco,overol,botasMaterial,botasCaucho,guantesCarnaza,guantesCaucho,guantesVaqueta,guantesNitrilo,protAuditivo,protAuditivoCopa,tapabocas,gafas,barbuquejo,firmaRRHH,firmaTrabajador,fechaFormulario) 
+        VALUES ('$nombres','$docAspirante','$docRecHum','$cargo','$proceso','$fehaEntrega','$casco','$overol','$botasMaterial','$botasCaucho','$guantesCarnaza','$guantesCaucho','$guantesVaqueta','$guantesNitrilo','$protAuditivo','$protAuditivoCopa','$tapabocas','$gafas','$barbuquejo','$firmaResponsable','$firmaTrabajador','$fechaFormulario')";
         mysqli_query($conn, $query_subirDatos);
 
         echo "<script> alert('El formulario de dotación fue cargado correctamente.'); window.location='../estadoLog/logtrue/recursosHumanos/formularios/dotacion.html' </script>";
@@ -40,19 +46,5 @@
     }
 
     insertar($conn);
-
-    /*if (isset($_POST['sendIndu'])) {
-        insertar($conn);
-        echo "<script> alert('El formulario de inducción fue cargado correctamente.'); window.location='../estadoLog/logtrue/recursosHumanos/formularios/induccion.html' </script>";
-    } else {
-        #echo "<h4>Error: </h4><br><h3>" . mysqli_error($conn) . "</h3>";
-    }
-    
-    if (isset($_POST['sendDota'])) {
-        insertar($conn);
-        echo "<script> alert('El formulario de dotación fue cargado correctamente.'); window.location='../estadoLog/logtrue/recursosHumanos/formularios/dotacion.html' </script>";
-    } else {
-        #echo "<h4>Error: </h4><br><h3>" . mysqli_error($conn) . "</h3>";
-    }*/
 
 ?>
--- HTML/php/formularioInduccion.php
+++ HTML/php/formularioInduccion.php
@@ -5,15 +5,29 @@
     #Identificar cada variable de vinculacion con la pagina y la base de datos.
     #Insertar a la base de datos los datos ingresados en cada campo del formulario.
 
-    function insertar ($conn){
-        
-        $docA=$_POST["docAspirante"];
-        $idFormulario=$_POST["idFormulario"];
-        $tipoF=$_POST["tipoFormulario"]; 
-        $fechaF=$_POST["fechaFormulario"]; 
-        $docRecHumS=$_POST["docRecHum"]; 
-        $archivoF=$_POST["archivoFormulario"]; 
-        
+    if(isset($_POST['sendIndu'])) {
+        insertar($conn);
+    } 
+
+    function insertar ($conn) {
+
+        $docAspirante = $_POST ["docAspirante"];
+        /* $docRecHum = $_POST["docRecHum"]; */
+        $objetivo = $_POST["objetivo"];
+        $fecha = $_POST["fecha"];
+        $hora = $_POST["hora"];
+        $lugar = $_POST["lugar"];
+        $oficinaPrincipal = $_POST["oficinaPrincipal"];
+        $responsables = $_POST["responsables"];
+        $cargoResponsable = $_POST["cargoResponsable"];
+        $tema = $_POST["tema"];
+        $numLista = $_POST["numLista"];
+        $nombresCompletos = $_POST["nombresCompletos"];
+        $cargoAspirante = $_POST["cargoAspirante"];
+        $firmaAspirante = $_POST["firmaAspirante"];
+        $firmaResponsable = $_POST["firmaResponsable"];
+
+
         $query_subirDatos= "INSERT INTO formulario (idFormulario,tipoFormulario,archivoFormulario,fechaFormulario,docRecHum,docAspirante) VALUES ('$idFormulario','$tipoF','$archivoF','$fechaF','$docRecHumS','$docA')";
         mysqli_query($conn, $query_subirDatos);
         
