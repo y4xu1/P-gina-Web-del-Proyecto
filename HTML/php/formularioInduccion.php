b50b36fc067e8eeb53e89c8496708cfe85ca6fcd
y4xul <yaxscardenas@misena.edu.co>
2022-03-25 19:44:19 -0500

    formularios
    No se ha concretado una función para los formularios (problemas con la base de datos).
    
    Co-Authored-By: Laura23-g <79876914+Laura23-g@users.noreply.github.com>

--- HTML/php/formularioInduccion.php
+++ HTML/php/formularioInduccion.php
@@ -9,32 +9,62 @@
         insertar($conn);
     } 
 
-    function insertar ($conn) {
+    //insertar($conn);
+    function insertar($conn) {
 
-        $docAspirante = $_POST ["docAspirante"];
-        /* $docRecHum = $_POST["docRecHum"]; */
-        $objetivo = $_POST["objetivo"];
-        $fecha = $_POST["fecha"];
-        $hora = $_POST["hora"];
-        $lugar = $_POST["lugar"];
-        $oficinaPrincipal = $_POST["oficinaPrincipal"];
-        $responsables = $_POST["responsables"];
-        $cargoResponsable = $_POST["cargoResponsable"];
-        $tema = $_POST["tema"];
-        $numLista = $_POST["numLista"];
-        $nombresCompletos = $_POST["nombresCompletos"];
-        $cargoAspirante = $_POST["cargoAspirante"];
-        $firmaAspirante = $_POST["firmaAspirante"];
-        $firmaResponsable = $_POST["firmaResponsable"];
+        $docAspirante = isset($_POST ["docAspirante"]);
+        $docRecHum = isset($_POST["docRecHum"]);
+        $objetivo = isset($_POST["objetivo"]);
+        $fecha = isset($_POST["fecha"]);
+        $hora = isset($_POST["hora"]);
+        $lugar = isset($_POST["lugar"]);
+        $oficinaPrincipal = isset($_POST["oficinaPrincipal"]);
+        $responsables = isset($_POST["responsables"]);
+        $cargoResponsable = isset($_POST["cargoResponsable"]);
+        $tema = isset($_POST["tema"]);
+        $numLista = isset($_POST["numLista"]);
+        $nombresCompletos = isset($_POST["nombresCompletos"]);
+        $cargoAspirante = isset($_POST["cargoAspirante"]);
+        //$firmaAspirante = isset($_POST["firmaAspirante"]);
+        //$firmaResponsable = isset($_POST["firmaResponsable"]);
 
+        //echo '<script>alert("si");</script>';
 
-        $query_subirDatos= "INSERT INTO formulario (idFormulario,tipoFormulario,archivoFormulario,fechaFormulario,docRecHum,docAspirante) VALUES ('$idFormulario','$tipoF','$archivoF','$fechaF','$docRecHumS','$docA')";
-        mysqli_query($conn, $query_subirDatos);
-        
-        echo "<script> alert('El formulario de inducción fue cargado correctamente.'); window.location='../estadoLog/logtrue/recursosHumanos/formularios/induccion.html' </script>";
+        $consulta = "INSERT INTO formulario_induccion (docAspirante, docRRHH, objetivo, fecha, hora, lugar, oficinaPrincipal, responsables, cargoResponsable, tema, numLista,
+        nombresCompletos, cargoAspirante) VALUES ('$docAspirante', '$docRecHum' '$objetivo', '$fecha', '$hora', '$lugar', '$oficinaPrincipal', '$responsables',
+        '$cargoResponsable', '$tema', '$numLista', '$nombresCompletos', '$cargoAspirante')"; //, firmaAspirante, firmaResponsable--, '$firmaAspirante', '$firmaResponsable'
+
+        //echo '<script>alert("si x2");</script>';
+
+        mysqli_query($conn, $consulta);
+
+        echo '<script>alert("si x3");</script>';
+
+        if (mysqli_query($conn, $consulta) == true) {
+            
+            echo "<script> alert('El formulario de inducción fue cargado correctamente.'); window.location='../estadoLog/logtrue/recursosHumanos/formularios/induccion.html' </script>";
+        }
+        else {
+
+            echo "<script> alert('fuck'); window.location='../estadoLog/logtrue/recursosHumanos/formularios/induccion.html' </script>";
+        }
         
         mysqli_close($conn);
     }
-    
-    insertar($conn);
+
+?>
+<!--
+< ?php
+
+
+for ($a==1; $a<=30; $a++) {
+    echo '  <tr>
+                <td><input class="control'. $a . '" type="text" name="numLista'. $a . '" id="numLista" value="'. $a . '" disabled></td>
+                <td><input class="control'. $a . '" type="text" name="nombresCompletos'. $a . '" id="nombresCompletos" placeholder="Pepito Pérez"></td>
+                <td><input class="control'. $a . '" type="text" name="cargoAspirante'. $a . '" id="cargoAspirante" placeholder="Asistencia"></td>
+                <td><input class="control'. $a . '" type="text" name="docAspirante'. $a . '" id="docAspirante" placeholder="9999999999"></td>
+                <td><input class="control'. $a . '" type="text" name="firmaAspirante'. $a . '" id="firmaAspirante" placeholder="Adjunte su firma dígital"></td>
+            </tr>';
+}
+
 ?>
